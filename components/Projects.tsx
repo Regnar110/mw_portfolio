81ee244000cf8322ef4a333f51c78aa34f7ab8ea
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-05-11 21:11:02 +0200

    fixed bug with firefox browser layout reading

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,10 +14,10 @@ const Projects = () => {
         <div className='header_neon relative flex justify-center mb-14 lg:mb-32'>
             <h1 className="text-white font-press neon-text text-3xl lg:text-4xl text-center">MY PROJECTS </h1>
         </div>
-        <div className='project_list font-press flex flex-col gap-5'>
-            <div className='single_project flex flex-col xl:flex-row overflow-hidden'>
+        <div className='project_list font-press w-full flex flex-col gap-5'>
+            <div className='single_project w-full flex flex-col xl:flex-row overflow-hidden'>
                 <Image className='w-full xl:w-1/2' src={apart} alt='apart' />
-                <div className='single_project_desc flex flex-col items-center gap-5  p-4'>
+                <div className='single_project_desc xl:w-1/2 flex flex-col items-center gap-5  p-4'>
                     <h1 className='text-[18px] md:text-[22px] text-[#017ACC]'>Apart Shop</h1>
                     <p className='h-full text-[10px] md:text-[12px] text-black text-center'>
                         This project is a simplified version of the apart store created with <span className='text-[#16A34A]'>Javascript</span>, <span className='text-[#16A34A]'>Typescript</span>, <span className='text-[#16A34A]'>React</span>, <span className='text-[#16A34A]'>Next.js</span>, <span className='text-[#16A34A]'>Redux Toolkit</span> and other support libraries.
@@ -38,7 +38,7 @@ const Projects = () => {
             </div>
             <div className='single_project flex flex-col xl:flex-row-reverse'>
                 <Image className='w-full xl:w-1/2' src={apple} alt='apart' />                             
-                <div className='single_project_desc flex flex-col items-center gap-5  p-4'>
+                <div className='single_project_desc xl:w-1/2 flex flex-col items-center gap-5  p-4'>
                     <h1 className='text-[18px] md:text-[22px] text-[#017ACC]'>Apple Shop</h1>
                     <p className='h-full text-[10px] md:text-[12px] text-black text-center'>
                         This project is a simplified version of the Apple Store. The project was created using <span className='text-[#16A34A]'>Javascript</span>, <span className='text-[#16A34A]'>Typescript</span>, <span className='text-[#16A34A]'>React</span>, <span className='text-[#16A34A]'>Next.js</span>, <span className='text-[#16A34A]'>Redux Toolkit</span> and other support libraries.
@@ -58,7 +58,7 @@ const Projects = () => {
             </div>
             <div className='single_project flex flex-col xl:flex-row'>
                 <Image className='w-full xl:w-1/2' src={weather} alt='apart' />                             
-                <div className='single_project_desc flex flex-col items-center gap-5  p-4'>
+                <div className='single_project_desc xl:w-1/2 flex flex-col items-center gap-5  p-4'>
                     <h1 className='text-[18px] md:text-[22px] text-[#017ACC]'>Weather App</h1>
                     <p className='h-full text-[10px] md:text-[12px] text-black text-center'>
                         Weather App is an application written using <span className='text-[#16A34A]'>React</span> and <span className='text-[#16A34A]'>Node.js</span> + <span className='text-[#16A34A]'>Express.js</span> backend. The application uses several Google programming interfaces such as GeoLocation, Reverse Geolocation, TimeZone, Places (for autocomplete searches for places) and the OpenMeteo API and Navigator API of the browser, which allows you to obtain the user's location.
@@ -78,7 +78,7 @@ const Projects = () => {
             </div>
             <div className='single_project flex flex-col xl:flex-row-reverse'>
                 <Image className='w-full xl:w-1/2' src={portfolio} alt='apart' />                             
-                <div className='single_project_desc flex flex-col items-center gap-5  p-4'>
+                <div className='single_project_desc xl:w-1/2 flex flex-col items-center gap-5  p-4'>
                     <h1 className='text-[18px] md:text-[22px] text-[#017ACC]'>Portfolio</h1>
                     <p className='h-full text-[10px] md:text-[12px] text-black text-center'>
                         A simple one-page website. Used using <span className='text-[#16A34A]'>React</span>. It introduces me as a developer and presents my projects.
